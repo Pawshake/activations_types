526f8bb7c90bf5c28cff4ca4a662e4ed74bcdef0
Ward Van Hooreweghe <ward.vanhooreweghe@calibrate.be>
2017-09-05 14:18:25 +0200

    master: updated the constants to include activations_.

--- Activations/Types.php
+++ Activations/Types.php
@@ -9,30 +9,30 @@ abstract class Types {
     /**
      *
      */
-    const RECOMMENDED_SITTERS = 'recommended_sitters';
+    const RECOMMENDED_SITTERS = 'activations_recommended_sitters';
 
     /**
      * @see https://pawshake.atlassian.net/browse/DEV-3865
      */
-    const HOW_TO_CREATE_A_BOOKING = 'how_to_create_a_booking';
+    const HOW_TO_CREATE_A_BOOKING = 'activations_how_to_create_a_booking';
 
     /**
      * @see https://pawshake.atlassian.net/browse/DEV-3868
      */
-    const HOW_TO_CREATE_AN_ENQUIRY = 'how_to_create_an_enquiry';
+    const HOW_TO_CREATE_AN_ENQUIRY = 'activations_how_to_create_an_enquiry';
 
     /**
      * @see https://pawshake.atlassian.net/browse/DEV-3862
      */
-    const RESPONSIVE_HOSTS_ENQUIRY = 'responsive_hosts_enquiry';
+    const RESPONSIVE_HOSTS_ENQUIRY = 'activations_responsive_hosts_enquiry';
 
     /**
      * @see https://pawshake.atlassian.net/browse/DEV-3863
      */
-    const RESPONSIVE_HOSTS_BOOKING = 'responsive_hosts_booking';
+    const RESPONSIVE_HOSTS_BOOKING = 'activations_responsive_hosts_booking';
 
     /**
      * @see https://pawshake.atlassian.net/browse/DEV-3864
      */
-    const MOTIVATE_GUEST_TO_SEND_MULTIPLE_ENQUIRIES = 'motivate_guest_to_send_multiple_enquiries';
+    const MOTIVATE_GUEST_TO_SEND_MULTIPLE_ENQUIRIES = 'activations_motivate_guest_to_send_multiple_enquiries';
 }
