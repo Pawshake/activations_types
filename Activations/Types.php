689424105b0a0884511c10c691d312a770215768
Ward Van Hooreweghe <ward.vanhooreweghe@calibrate.be>
2017-09-05 11:20:12 +0200

    master: added the types that we have found in the tickets for now.

---
+++ Activations/Types.php
@@ -0,0 +1,38 @@
+<?php namespace Activations;
+
+/**
+ * Class Types
+ * @package Activations
+ */
+abstract class Types {
+
+    /**
+     *
+     */
+    const RECOMMENDED_SITTERS = 'recommended_sitters';
+
+    /**
+     * @see https://pawshake.atlassian.net/browse/DEV-3865
+     */
+    const HOW_TO_CREATE_A_BOOKING = 'how_to_create_a_booking';
+
+    /**
+     * @see https://pawshake.atlassian.net/browse/DEV-3868
+     */
+    const HOW_TO_CREATE_AN_ENQUIRY = 'how_to_create_an_enquiry';
+
+    /**
+     * @see https://pawshake.atlassian.net/browse/DEV-3862
+     */
+    const RESPONSIVE_HOSTS_ENQUIRY = 'responsive_hosts_enquiry';
+
+    /**
+     * @see https://pawshake.atlassian.net/browse/DEV-3863
+     */
+    const RESPONSIVE_HOSTS_BOOKING = 'responsive_hosts_booking';
+
+    /**
+     * @see https://pawshake.atlassian.net/browse/DEV-3864
+     */
+    const MOTIVATE_GUEST_TO_SEND_MULTIPLE_ENQUIRIES = 'motivate_guest_to_send_multiple_enquiries';
+}
